10e6948fb5ab4646c4a414bf0d60a345d19b19dd
Dennis <mail@dwolt.de>
2015-01-23 19:24:55 +0100

    Modifications to the example program, so that a TriState code can be passed as an argument.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # About
 
-rcswitch-pi is for controlling rc remote controlled power sockets 
-with the raspberry pi. Kudos to the projects [rc-switch](http://code.google.com/p/rc-switch)
-and [wiringpi](https://projects.drogon.net/raspberry-pi/wiringpi).
-I just adapted the rc-switch code to use the wiringpi library instead of
-the library provided by the arduino.
+This is a fork of [rcswitch-pi](https://github.com/r10r/rcswitch-pi). The send.cpp was altered so that a TriState code can be passed as an argument.
 
 
 ## Usage
 
-First you have to install the [wiringpi](https://projects.drogon.net/raspberry-pi/wiringpi/download-and-install/) library.
-After that you can compile the example program *send* by executing *make*. 
-You may want to changet the used GPIO pin before compilation in the send.cpp source file.
+First you have to install the [wiringpi](http://wiringpi.com/download-and-install/) library.
+Afterwards, the example program can be compile be executing *make*.
+You may want to change the used GPIO pin before compilation in the send.cpp source file. By default GPIO17 will be used for sending data to the 433Mhz transmitter.
+
+The example program can be used as follows:
+```
+sudo ./send <TriStateCode>
+```
+Of course, *<TriStateCode>* must be replaced by a proper TriState code, e.g. *000000000FFF* (A01 On) or *000000000FF0* (A01 Off). Information on TriState codes can be found in the [FHEM wiki](http://www.fhemwiki.de/wiki/Intertechno_Code_Berechnung) (only available in German).
--- send.cpp
+++ send.cpp
@@ -1,39 +1,37 @@
 /*
- Usage: ./send <systemCode> <unitCode> <command>
- Command is 0 for OFF and 1 for ON
- */
+ Usage: ./send <TriStateCode>
+*/
 
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    
-    /*
-     output PIN is hardcoded for testing purposes
-     see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-     for pin mapping of the raspberry pi GPIO connector
-     */
-    int PIN = 0;
-    char* systemCode = argv[1];
-    int unitCode = atoi(argv[2]);
-    int command  = atoi(argv[3]);
-    
-    if (wiringPiSetup () == -1) return 1;
-	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
-	RCSwitch mySwitch = RCSwitch();
-	mySwitch.enableTransmit(PIN);
-    
-    switch(command) {
-        case 1:
-            mySwitch.switchOn(systemCode, unitCode);
-            break;
-        case 0:
-            mySwitch.switchOff(systemCode, unitCode);
-            break;
-        default:
-            printf("command[%i] is unsupported\n", command);
-            return -1;
+  int PIN = 0;
+
+  if(wiringPiSetup () == -1)  {
+    printf("Setting up WiringPi failed.\n");
+    return -1;
+  }
+
+  if(argc < 2) {
+    printf("Usage: ./send <TriStateCode>");
+    return -1;
+  }
+
+  char* code = argv[1];
+  int length = strlen(code);
+  for(int i = 0; i < length; i++) {
+    if(code[i] != '0' && code[i] != '1' && code[i] != 'F') {
+      printf("Argument is not a TriState code: %s\n", code);
+      return -1;
     }
-	return 0;
+  }
+  RCSwitch mySwitch = RCSwitch();
+  mySwitch.enableTransmit(PIN);
+
+  printf("Sending code: %s\n", code);
+  mySwitch.sendTriState(code);  
+  return 0;
 }
