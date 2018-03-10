0b4fe9ae5981bf803cd40ba9c024a9e31beec994
Dennis Wolters <mail@dwolt.de>
2018-03-10 01:31:53 +0100

    Altered CLI to work with OpenHAB's exec binding

--- send.cpp
+++ send.cpp
@@ -16,22 +16,32 @@ int main(int argc, char *argv[]) {
   }
 
   if(argc < 2) {
-    printf("Usage: ./send <TriStateCode>");
+    printf("Usage: ./send <TriStateCode> [<ON/OFF>]");
     return -1;
   }
 
-  char* code = argv[1];
-  int length = strlen(code);
-  for(int i = 0; i < length; i++) {
+  int length = strlen(argv[1]);
+  char code[length+4];
+  int i = 0;
+  for(; i < length; i++) {
+    code[i] = argv[1][i];
     if(code[i] != '0' && code[i] != '1' && code[i] != 'F') {
       printf("Argument is not a TriState code: %s\n", code);
       return -1;
     }
   }
+  code[i++] = '0';
+  code[i++] = 'F';
+  code[i++] = 'F';
+
+  if(strcmp(argv[2],"ON") == 0 || strcmp(argv[2],"100") == 0)
+    code[i++] = 'F';
+  else
+    code[i++] = '0';
   RCSwitch mySwitch = RCSwitch();
   mySwitch.enableTransmit(PIN);
 
   printf("Sending code: %s\n", code);
-  mySwitch.sendTriState(code);  
+  mySwitch.sendTriState(code);
   return 0;
 }
